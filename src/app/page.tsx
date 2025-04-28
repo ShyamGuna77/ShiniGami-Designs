b71f81e9245378c40f1fd533cee58697c10b4812
Shyam <gshyamprasad2001@gmail.com>
2025-04-28 14:35:56 +0530

    Inir

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+     <Button variant="ghost" className="p-4"
+     >Click Me
+     </Button>
+    </div>
+  );
+}
