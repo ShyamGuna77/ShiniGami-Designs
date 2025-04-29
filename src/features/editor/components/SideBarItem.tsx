da1e5bec6cd3dd53f4b5ffdc4805297411496b91
Shyam <gshyamprasad2001@gmail.com>
2025-04-29 11:55:06 +0530

    sidebar issue

--- src/features/editor/components/SideBarItem.tsx
+++ src/features/editor/components/SideBarItem.tsx
@@ -1,25 +1,32 @@
+import type { LucideIcon } from "lucide-react";
 
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
-import { LucideIcon } from 'lucide-react'
-import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
-import React from 'react'
-
-
-type SideBarItemProps = {
-    icon: LucideIcon
-    label: string
-    isActive:boolean
-    onClick: () => void
+interface SidebarItemProps {
+  icon: LucideIcon;
+  label: string;
+  isActive?: boolean;
+  onClick: () => void;
 }
 
-const SideBarItem = ({icon:Icon,label,isActive,onClick}: SideBarItemProps) => {
+export const SideBarItem = ({
+  icon: Icon,
+  label,
+  isActive,
+  onClick,
+}: SidebarItemProps) => {
   return (
-    <Button variant= "ghost"  onClick={onClick} className={cn('w-full h-full aspect-video p-3 py-4 flex flex-col rounded-none ',isActive && 'bg-muted font-medium text-primary')}>
-        <Icon className='size-5 mt-2 stroke-2 shrink-0'/>
-        <span className=' text-xs'>{label}</span>
+    <Button
+      variant="ghost"
+      onClick={onClick}
+      className={cn(
+        "w-full h-full aspect-video p-3 py-4 flex flex-col rounded-none",
+        isActive && "bg-muted text-primary"
+      )}
+    >
+      <Icon className="size-5 stroke-2 shrink-0" />
+      <span className="mt-2 text-xs">{label}</span>
     </Button>
-  )
-}
-
-export default SideBarItem
+  );
+};
--- src/features/editor/components/Sidebar.tsx
+++ src/features/editor/components/Sidebar.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import React from "react";
-import SideBarItem from "./SideBarItem";
-import { Palette } from "lucide-react";
+import {SideBarItem} from "./SideBarItem";
+import {
+  Palette,
+  ImageIcon,
+  Pencil,
+ 
+  Settings,
+  Shapes,
+  Type,
+  Sparkles,
+} from "lucide-react";
 
 const Sidebar = () => {
   return (
@@ -11,7 +20,45 @@ const Sidebar = () => {
         <SideBarItem
           icon={Palette}
           label="Design"
-          isActive={true}
+          isActive={false}
+          onClick={() => {}}
+        />
+
+        <SideBarItem
+          icon={ImageIcon}
+          label="Image"
+          isActive={false}
+          onClick={() => {}}
+        />
+        <SideBarItem
+          icon={Type}
+          label="Text"
+          isActive={false}
+          onClick={() => {}}
+        />
+        <SideBarItem
+          icon={Shapes}
+          label="Shapes"
+          isActive={false}
+          onClick={() => {}}
+        />
+        <SideBarItem
+          icon={Pencil}
+          label="Draw"
+          isActive={false}
+          onClick={() => {}}
+        />
+        <SideBarItem
+          icon={Sparkles}
+          label="AI"
+          isActive={false}
+          onClick={() => {}}
+        />
+
+        <SideBarItem
+          icon={Settings}
+          label="Settings"
+          isActive={false}
           onClick={() => {}}
         />
       </ul>
