9f7b52f074712ce9e731de6cd2f6af0542c1b79a
Shyam <gshyamprasad2001@gmail.com>
2025-04-29 15:44:52 +0530

    Added Shapes sidebar ad Tool sidebar Header

--- src/features/editor/components/ShapeSideBar.tsx
+++ src/features/editor/components/ShapeSideBar.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react'
 import { ActiveTool } from '../types'
+import {cn} from '@/lib/utils'
 
 type ShapeSideBarProps = {
     activeTool :ActiveTool;
@@ -11,8 +12,17 @@ type ShapeSideBarProps = {
 
 const ShapeSideBar = ({activeTool,onChangeActiveTool}:ShapeSideBarProps) => {
   return (
-    <div>ShapeSideBar</div>
-  )
+    <aside
+      className={cn(
+        "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
+        activeTool ===  "shapes" ? "visible" : "hidden"
+      )}
+    >
+
+   Shapes Side bar
+
+    </aside>
+  );
 }
 
 export default ShapeSideBar
