907561b6350fdcae17f962ec24d89831d7fd3dc6
Shyam <gshyamprasad2001@gmail.com>
2025-05-01 11:25:24 +0530

    added images api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
 
 
 import {Hono} from 'hono'
+import images from "./images"
 
 import {handle} from 'hono/vercel'
 
@@ -10,26 +11,7 @@ const app = new Hono().basePath("/api");
 
 
 
-app.get("/test", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
-
-const routes =app.get("/posts/:id", (c) => {
-  const page = c.req.query("page");
-  const id = c.req.param("id");
-  c.header("X-Message", "Hi!");
-  return c.text(`You want to see ${page} of ${id}`);
-});
-
-
- const routes = app.get("/tests/:id" , (e) => {
-    const test = e.req.query("test")
-    const id  = e.req.param("id")
-    return e.text(`Hey There this is  from test you want to see ${test} of ${id}`)   
-})
-
+const routes = app.route("/images",images)
 
 
 export const GET = handle(app);
