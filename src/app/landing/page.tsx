aabae9986d819b9ce93e4db44cfeea05e18a28d7
Shyam <gshyamprasad2001@gmail.com>
2025-09-06 11:23:52 +0530

    fix

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -5,7 +5,7 @@ import { ProjectsSection } from "../(dashboard)/project-section";
 import { TemplatesSection } from "../(dashboard)/template-section";
 
 export default async function Home() {
-  // await protectServer();
+  await protectServer();
 
   return (
     <div className="flex flex-col space-y-4 px-4 sm:space-y-6 sm:px-6 lg:px-8 max-w-screen-xl mx-auto pb-6 sm:pb-10">
